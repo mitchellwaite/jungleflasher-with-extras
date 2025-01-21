992fadc1f88a0b3dfb25124083a252c2f922db20
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-01-20 21:47:39 -0500

    Clean up Donate form

---
+++ JungleFlasher/Forms/Donate.cs
@@ -0,0 +1,148 @@
+﻿// Type: ns0.Donate
+// Assembly: JungleFlasher, Version=1.0.5004.40451, Culture=neutral, PublicKeyToken=7240d12d147b2b97
+// MVID: 86B778B8-1293-454B-A759-8F0EBE32074A
+// Assembly location: C:\Documents and Settings\User\Desktop\JungleFlasher-cleaned.exe
+
+using System.ComponentModel;
+using System.Drawing;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ns0
+{
+  public class Donate : Form
+  {
+    private PictureBox pictureBox1;
+    private Button button1;
+    private Button button2;
+    private Label label10;
+    private Label label11;
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private Container container_0;
+
+    public Donate()
+    {
+      this.InitializeComponent();
+    }
+
+    protected override void Dispose([MarshalAs(UnmanagedType.U1)] bool disposing)
+    {
+      if (disposing && this.container_0 != null)
+        this.container_0.Dispose();
+      base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Donate));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 38);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(136, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Yes";
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(224, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "No";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(104, 16);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(320, 16);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "As a heavy user of JungleFlasher, Maybe you would";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(104, 32);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(320, 16);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "consider a donation to the dev Team, as a sign of";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(104, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "appreciation and recognition of all the hard work and";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(104, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(320, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "dedication that goes into developing such a full";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(104, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(320, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "featured application.";
+            // 
+            // Donate
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(434, 144);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Donate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Donate";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+    }
+  }
+}

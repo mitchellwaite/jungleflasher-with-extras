175f19f4c9a4bec0ed176b9830ec6c7c039242ba
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-01-20 21:04:49 -0500

    Clean up Welcome form

---
+++ JungleFlasher/Forms/Welcome.cs
@@ -0,0 +1,142 @@
+﻿// Type: ns0.Welcome
+// Assembly: JungleFlasher, Version=1.0.5004.40451, Culture=neutral, PublicKeyToken=7240d12d147b2b97
+// MVID: 86B778B8-1293-454B-A759-8F0EBE32074A
+// Assembly location: C:\Documents and Settings\User\Desktop\JungleFlasher-cleaned.exe
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ns0
+{
+  public class Welcome : Form
+  {
+    private static Timer timer_0 = new Timer();
+    private Label label1;
+    private LinkLabel linkLabel2;
+    private GroupBox groupBox1;
+    private PictureBox pictureBox1;
+    private Container container_0;
+
+    static Welcome()
+    {
+    }
+
+    public Welcome()
+    {
+      this.InitializeComponent();
+    }
+
+    protected override void Dispose([MarshalAs(UnmanagedType.U1)] bool disposing)
+    {
+      if (disposing && this.container_0 != null)
+        this.container_0.Dispose();
+      base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(69, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "JungleFlasher 0.1.96 b";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(18, 80);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(344, 16);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "For Support join #JungleFlasher on EFnet";
+            this.linkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(3, -2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 128);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(320, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Welcome
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(380, 128);
+            this.ControlBox = false;
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Welcome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.Welcome_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+    }
+
+    private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      this.method_0();
+    }
+
+    private void method_0()
+    {
+      this.linkLabel2.LinkVisited = true;
+      Process.Start("irc://irc.efnet.org/JungleFlasher");
+    }
+
+    private void Welcome_Load(object sender, EventArgs e)
+    {
+      Welcome.timer_0.Tick += new EventHandler(this.method_1);
+      Welcome.timer_0.Interval = 4000;
+      Welcome.timer_0.Start();
+    }
+
+    private void method_1(object sender, EventArgs e)
+    {
+      Welcome.timer_0.Stop();
+      this.Close();
+    }
+  }
+}

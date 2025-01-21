1b3046456b5d03486d2519259b9c35ef4c485381
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-01-20 21:02:11 -0500

    Clean up WorkingFolder

---
+++ JungleFlasher/Forms/WorkingFolder.cs
@@ -0,0 +1,115 @@
+﻿// Type: ns0.WorkingFolder
+// Assembly: JungleFlasher, Version=1.0.5004.40451, Culture=neutral, PublicKeyToken=7240d12d147b2b97
+// MVID: 86B778B8-1293-454B-A759-8F0EBE32074A
+// Assembly location: C:\Documents and Settings\User\Desktop\JungleFlasher-cleaned.exe
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ns0
+{
+  public class WorkingFolder : Form
+  {
+    private Button button3;
+    private Button button1;
+    public TextBox textBox8;
+    private Button button4;
+    private Container container_0;
+
+    public WorkingFolder()
+    {
+      this.InitializeComponent();
+    }
+
+    protected override void Dispose([MarshalAs(UnmanagedType.U1)] bool disposing)
+    {
+      if (disposing && this.container_0 != null)
+        this.container_0.Dispose();
+      base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorkingFolder));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Location = new System.Drawing.Point(101, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 24);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Cancel";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(205, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(64, 16);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(304, 20);
+            this.textBox8.TabIndex = 28;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Location = new System.Drawing.Point(16, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 20);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "...";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // WorkingFolder
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(386, 94);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WorkingFolder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Set Working Folder";
+            this.Load += new System.EventHandler(this.WorkingFolder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+      folderBrowserDialog.SelectedPath = this.textBox8.Text;
+      if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+        return;
+      this.textBox8.Text = Path.GetFullPath(folderBrowserDialog.SelectedPath) + "\\";
+    }
+
+    private void WorkingFolder_Load(object sender, EventArgs e)
+    {
+    }
+  }
+}
